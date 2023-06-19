9b0bdaf75eee491939fa200bcbb6bca64ff24774
moff <moffahr5@gmail.com>
2023-06-19 02:04:33 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: this program prints a statement and terminates to a new line
+ * Return: always (0), indicating sucess
+ */
+
+int main(void) /*the mins is void*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
