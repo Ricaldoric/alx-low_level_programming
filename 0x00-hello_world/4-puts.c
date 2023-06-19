a3d834f5ec7aa62608accbf1af9295cb393478a8
moff <moffahr5@gmail.com>
2023-06-19 02:26:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main- entry of the program
+ *
+ * Description: this program prints using 'puts rather than 'printf'
+ *
+ * Return:  always (0) indicating success
+ */
+
+int main(void)
 {
 	char message[] = "Programming is like building a multilingual puzzle";
+
 	puts(message);
 
-	return(0);
+	return (0);
 }
