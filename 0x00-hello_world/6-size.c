b0d82e0092d3c7d1ebc442e6933431ea71c2e523
moff <moffahr5@gmail.com>
2023-06-19 05:32:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,19 +2,20 @@
 /**
  * main - start of the program
  *
- * Description: 'the program prints the size of various types on the computer it is compiled and run on'
+ * Description: the program prints the size of various types on
  *
- * return: always (0) indicating success
+ * Return: always (0), indicating success
  */
 
-int main(void)
+int main(void) /*the program will display the sze of characters*/
 {
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
+	/*return 0 indicates the end of the program and it was successful*/
 	return (0);
 }
