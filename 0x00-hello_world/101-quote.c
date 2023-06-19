c4c370e522caa6739f383b1a5c829635cce91c12
moff <moffahr5@gmail.com>
2023-06-19 06:04:58 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the start of the program
+ *
+ * Descriprion: prints something
+ *
+ * Return:  (1), indicating fail
+ */
+
+int main(void)
+{
+	fprintf(stderr, "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
+
