1b7d31d170ab1445d2828c9ab6d4551095060365
moff <moffahr5@gmail.com>
2023-06-21 06:51:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,10 @@
  */
 void print_alphabet(void)
 {
-    for (char letter = 'a'; letter <= 'z'; ++letter)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');
+	for (char letter = 'a'; letter <= 'z'; ++letter)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }
 
