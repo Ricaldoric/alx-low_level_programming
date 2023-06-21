0a22cbeb8d199ba18487eeeb036d06b9a855af25
moff <moffahr5@gmail.com>
2023-06-21 06:48:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - print all the alphabets in lower case
+ */
+void print_alphabet(void)
+{
+    for (char letter = 'a'; letter <= 'z'; ++letter)
+    {
+        _putchar(letter);
+    }
+    _putchar('\n');
+}
+
