3f129ada89b0bca6f7148a734e0406dbfbb98bbd
moff <moffahr5@gmail.com>
2023-06-22 04:04:28 -0700

    message

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - program entry
+ *
+ * Return: 0, indicating success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	postitive_or_negative();
+
+	return (0);
+}
+
