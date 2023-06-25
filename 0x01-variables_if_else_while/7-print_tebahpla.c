e01dccd9d06eac61fe7b28df977bf23ee5415bf8
moff <moffahr5@gmail.com>
2023-06-25 02:34:35 -0700

    hgdd c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* main - program entry
+ *
+ * Description: prints alphabets in reverse order
+ *
+ * Return: (0), indicating success
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c => 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return(0);
+}
