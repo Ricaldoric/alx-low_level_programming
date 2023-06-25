0012f7211ac9c5fab55e40ff2b5edde079ec3b3a
moff <moffahr5@gmail.com>
2023-06-25 09:30:24 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - program entry point
+ *
+ * Descriprion: prints all the hexagonal numbers base 16
+ *
+ * Return: always (0), success
+ */
+int main(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(0 + i);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	return (0);
+}
+
