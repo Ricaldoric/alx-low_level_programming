6b983a80653b56e473b663084eefb6655c9646a7
moff <moffahr5@gmail.com>
2023-06-25 00:54:41 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * description: prints alphabets a-z using putchar except q and e
+ *
+ * Return: Always (0), success
+ */
+int main(void) /*start of the prohgram*/
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if c == 'q'
+			continue;
+		if c == 'e'
+			continue;
+		else
+		{
+		putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
+
