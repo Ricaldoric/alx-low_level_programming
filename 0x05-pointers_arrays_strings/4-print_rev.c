b2650d943f1d616b000c8a05f2d6425a0d6a4194
moff <moffahr5@gmail.com>
2023-06-26 19:49:43 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in the reverse order
+ * @s: string to be printed in reverse
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
