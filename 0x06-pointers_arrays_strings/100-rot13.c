54649c063e7d0bebc57bdd2e87b1a3ac27b45cbd
moff <moffahr5@gmail.com>
2023-06-29 08:22:47 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,15 +1,26 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * main - check the code
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
  *
- * Return: Always 0.
+ * Return: address of s
  */
-int main(void)
+char *rot13(char *s)
 {
-	char *a;
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	a = rot13("Hello, World");
-	printf("%s\n", a);
-	return (0);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
