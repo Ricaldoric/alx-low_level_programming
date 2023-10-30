80992845eb2861f990ed598787a6adbc2afcbc2b
moffat <moffahr5@gmail.com>
2023-10-30 09:29:57 -0700

    hello moff

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector (Unused)
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	/* Print the number of arguments passed 
+	 * to the program minus one (to exclude the program name)
+	 */
+	printf("%d\n", argc - 1);
+	return (0); /* Indicate successful execution */
+}
+
