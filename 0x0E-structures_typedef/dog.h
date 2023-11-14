a63447c6d41076696045d4691df9d0a2b6ceaf36
moffat <moffahr5@gmail.com>
2023-11-13 23:38:44 -0800

    hello

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,29 +2,16 @@
 #define DOG_H
 
 /**
- * struct dog - A new type representing a dog.
- * @name: String representing the dog's name.
- * @age: Float representing the dog's age.
- * @owner: String representing the name of the dog's owner.
+ * struct dog - a new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
-    char *name;  /* Name of the dog */
-    float age;   /* Age of the dog */
-    char *owner; /* Owner of the dog */
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif /* DOG_H */
-
-/* Explanation:
-   - The struct dog defines a new structure type with three members.
-   - @name is a pointer to char, representing the dog's name.
-   - @age is a float, representing the dog's age.
-   - @owner is a pointer to char, representing the dog's owner.
-   - This structure allows for creating variables that can store 
-     all these pieces of information about a dog together.
-   - Include guards (#ifndef, #define, #endif) prevent multiple inclusions 
-     of this header file in a project, ensuring that the compiler processes 
-     this definition only once regardless of how many times it's included.
-*/
-
