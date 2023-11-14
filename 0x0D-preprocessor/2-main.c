57fb50d96cb66b37de46f6dfb9234fcdb7a018cb
moffat <moffahr5@gmail.com>
2023-11-13 23:02:03 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/* Main function: Entry point of the program */
+/**
+ * main - Start of the program
+ * Description: Prints the name of the file it was compiled from
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/**
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/* ABS(x): Computes the absolute value of x. */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* FUNCTION_LIKE_MACRO_H */
+
